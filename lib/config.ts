2b52430af400cf6e76756a26a7696b19f40d2af2
tuantran0707 <tuan.tran@nube-io.com>
2025-12-07 00:35:14 +0700

    fix ui

---
+++ lib/config.ts
@@ -0,0 +1,9 @@
+const config = {
+  // tbServer: "thingsboard.cloud",
+  // tbServer: "212.38.94.144:8081",
+  tbServer: "thingsboard.cloud",
+  // tbServer: "thingsboard.srv524038.hstgr.cloud",
+  deviceId: "743b0d20-a527-11f0-b150-2710a8915e1d",
+};
+
+export { config };
