3685957de548c3b925322e0b6881cd1dab239000
tuantran0707 <tuan.tran@nube-io.com>
2025-12-07 01:01:29 +0700

    fix project

--- iot-web/app/layout.tsx
+++ iot-web/app/layout.tsx
@@ -6,8 +6,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "IoT Agricultural",
-  description: "IoT Agricultural control & monitoring via ThingsBoard",
+  title: "IoT Dashboard Monitor",
+  description: "IoT Dashboard Monitor using Thingsboard",
 };
 
 export default function RootLayout({
